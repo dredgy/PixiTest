e3d61cb6f89e3d3e2f93cb97c22fe6d04929279b
SamHope1992 <sampfhope@gmail.com>
2024-01-28 21:14:09 +1000

    There, I ruined it

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import * as PIXI from 'pixi.js'
 import { Viewport } from 'pixi-viewport'
+import ElementWrapper from './element-wrapper.js';
 
 const app = new PIXI.Application<HTMLCanvasElement>({
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     backgroundColor: 0xAAAAAA, // Set the background color
 })
 
@@ -25,19 +26,33 @@ viewport
     .drag()
     .pinch()
     .wheel()
-    .decelerate()
+    .decelerate({friction: 0.8})
 
-const newSquare = (x: number, y: number, color: PIXI.ColorSource) => {
+const newSquare = (x: number, y: number, color: PIXI.ColorSource):PIXI.Graphics => {
     const square = new PIXI.Graphics()
-
     square.beginFill(color)
     square.drawRect(x, y, 50, 50)
     square.endFill()
     square.interactive = true
     viewport.addChild(square)
-
+    return square;
 }
 
-
 newSquare(50, 50, 0xFF0000)
-newSquare(100, 100, 0x0000FF)
+let box = newSquare(100, 100, 0x0000FF)
+
+const element = document.getElementById('my-element');
+const wrappedElement = new ElementWrapper(element);
+app.stage.addChild(wrappedElement);
+
+app.ticker.add((delta) => {
+    if(box.x < 1500){
+        box.x += 1}
+    else {
+        box.x = 1;
+    }
+
+    let t = viewport.toScreen(new PIXI.Point(box.x, box.y))
+    wrappedElement.x = t.x;
+    wrappedElement.y = t.y;
+});
